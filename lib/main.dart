621037a98b292f5ae9924b4da209e40c11e4f791
michael.boumansour <michael.boumansour@agilecreatives.net>
2021-06-28 17:32:45 -0400

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage() : super();
+
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  int diceNumber = 1;
+  int diceNumber2 = 2;
+  void updateDice() {
+    setState(() {
+      diceNumber = Random().nextInt(6) + 1;
+      diceNumber2 = Random().nextInt(6) + 1;
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: TextButton(
+              onPressed: () {
+                updateDice();
+              },
+              child: Image.asset('images/dice$diceNumber.png'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: TextButton(
+              onPressed: () {
+                updateDice();
+              },
+              child: Image.asset('images/dice$diceNumber2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
